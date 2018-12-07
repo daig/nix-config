4213247614959364896d99dd049ff147e1a9e05c
Dai <daig@noreply.github.com>
2018-12-07 16:17:33 +0000

    add wireless support via wpa_supplicant

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,7 @@ with builtins;
 
 
   # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
